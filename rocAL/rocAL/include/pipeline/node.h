4e32391a8432d5cd674d06c364a666de9125014f
fiona-gladwin <fionagladwin@multicorewareinc.com>
2022-12-16 04:46:06 -0500

    Use vx_tensors for src and dst in node
    Create vs tensors with the ROI buffer

--- rocAL/rocAL/include/pipeline/node.h
+++ rocAL/rocAL/include/pipeline/node.h
@@ -24,34 +24,33 @@ THE SOFTWARE.
 #include <set>
 #include <memory>
 #include "graph.h"
-#include "image.h"
+#include "tensor.h"
 #include "meta_data_graph.h"
 class Node
 {
 public:
-    Node(const std::vector<Image *> &inputs, const std::vector<Image *> &outputs) :
+    Node(const std::vector<rocalTensor *> &inputs, const std::vector<rocalTensor *> &outputs) :
         _inputs(inputs),
         _outputs(outputs),
         _batch_size(outputs[0]->info().batch_size()) {}
     virtual ~Node();
     void create(std::shared_ptr<Graph> graph);
     void update_parameters();
-    std::vector<Image*> input() { return _inputs; };
-    std::vector<Image*> output() { return _outputs; };
-    void add_next(const std::shared_ptr<Node>& node) {} // To be implemented
-    void add_previous(const std::shared_ptr<Node>& node) {} //To be implemented
+    std::vector<rocalTensor *> input() { return _inputs; };
+    std::vector<rocalTensor *> output() { return _outputs; };
+    void add_next(const std::shared_ptr<rocalTensor>& node) {} // To be implemented
+    void add_previous(const std::shared_ptr<rocalTensor>& node) {} //To be implemented
     std::shared_ptr<Graph> graph() { return _graph; }
     void set_meta_data(MetaDataBatch* meta_data_info){_meta_data_info = meta_data_info;}
     bool _is_ssd = false;
 protected:
     virtual void create_node() = 0;
     virtual void update_node() = 0;
-    virtual void update_src_roi();
-    std::vector<Image*> _inputs;
-    std::vector<Image*> _outputs;
+    const std::vector<rocalTensor *> _inputs;
+    const std::vector<rocalTensor *> _outputs;
     std::shared_ptr<Graph> _graph = nullptr;
-    vx_array _src_roi_width = nullptr;
-    vx_array _src_roi_height = nullptr;
+    vx_tensor _src_tensor_roi = nullptr;
+    vx_tensor _dst_tensor_roi = nullptr;
     vx_node _node = nullptr;
     size_t _batch_size;
     MetaDataBatch* _meta_data_info;
