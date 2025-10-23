82b54f8bf6b117e6a4727dc45868fa8334e92adb
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-10-23 11:20:28 -0500

    Add OpenVX extension for RPP erase

--- amd_openvx_extensions/amd_rpp/include/internal_publishKernels.h
+++ amd_openvx_extensions/amd_rpp/include/internal_publishKernels.h
@@ -177,6 +177,7 @@ vx_status GaussianFilter_Register(vx_context);
 vx_status MedianFilter_Register(vx_context);
 vx_status WarpPerspective_Register(vx_context);
 vx_status Threshold_Register(vx_context);
+vx_status Erase_Register(vx_context);
 
 // kernel names
 #define VX_KERNEL_RPP_NOPBATCHPD_NAME                           "org.rpp.NopbatchPD"
@@ -321,5 +322,6 @@ vx_status Threshold_Register(vx_context);
 #define VX_KERNEL_RPP_MEDIAN_FILTER_NAME                        "org.rpp.MedianFilter"
 #define VX_KERNEL_RPP_WARP_PERSPECTIVE_NAME                     "org.rpp.WarpPerspective"
 #define VX_KERNEL_RPP_THRESHOLD                                 "org.rpp.Threshold"
+#define VX_KERNEL_RPP_ERASE                                     "org.rpp.Erase"
 
 #endif //_AMDVX_EXT__PUBLISH_KERNELS_H_
--- amd_openvx_extensions/amd_rpp/source/internal_publishKernels.cpp
+++ amd_openvx_extensions/amd_rpp/source/internal_publishKernels.cpp
@@ -182,6 +182,7 @@ vx_status get_kernels_to_publish()
     STATUS_ERROR_CHECK(ADD_KERNEL(MedianFilter_Register));
     STATUS_ERROR_CHECK(ADD_KERNEL(WarpPerspective_Register));
     STATUS_ERROR_CHECK(ADD_KERNEL(Threshold_Register));
+    STATUS_ERROR_CHECK(ADD_KERNEL(Erase_Register));
     return status;
 }
 
